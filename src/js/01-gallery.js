a59f36bf5cecf5503f9f25e7d879ca4b28b89c19
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2021-12-22 20:35:07 +0200

    робоча версія

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const galleryPic = galleryItems
+  .map(
+    galleryItem =>
+      `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`,
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryPic);
+
+const openOriginal = function (event) {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
+  event.preventDefault();
+};
+
+gallery.addEventListener('click', openOriginal);
